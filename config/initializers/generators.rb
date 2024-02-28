8c34ab60f2ff12880713c22e0e4cab92aa919abc
Anne Bortoli <annebortoli@gmail.com>
2024-02-28 16:24:44 -0300

    :heavy_plus_sign: add gem friendlyId to identify articles

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-Rails.application.config.generators do |_generator|
-    generator.orm(:active_record, primary_key_type: :uuid)
+Rails.application.config.generators do |generator|
+  generator.orm(:active_record, primary_key_type: :uuid)
 end

b47bae47ca1762ded54493b60f325ffc06c5cda3
Anne Bortoli <annebortoli@gmail.com>
2024-04-29 13:31:15 -0300

    added column name to user Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 # Create default admin users
-admins = ["admin@admin.com", "test@test.com"]
+admins = {
+  "admin@admin.com" => "Administrador",
+  "test@test.com" => "Teste",
+}
 
-admins.each do |email|
+admins.each do |email, name|
   next if Admin.find_by(email: email)
 
   Admin.create!(
     email:,
+    name:,
     password: ENV["DEFAULT_PASSWORD"],
     password_confirmation: ENV["DEFAULT_PASSWORD"],
   )

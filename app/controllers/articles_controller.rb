8c28fdf2dba5b1090ce7fbbb30a0667368a903fd
Anne Bortoli <annebortoli@gmail.com>
2024-07-15 11:23:02 -0300

    adjustin menu links and other posts links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,7 @@
 class ArticlesController < ApplicationController
   def show
     @article = Article.friendly.find(params[:id])
+    @other_articles = Article.all.sample(3)
+    # @other_articles = Article.all.take(3) # same article always
   end
 end

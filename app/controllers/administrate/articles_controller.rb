4720c9c9105c5b1f7690174ad63cac57815e6274
Anne Bortoli <annebortoli@gmail.com>
2024-07-17 12:51:50 -0300

    Added author to be edit in article

--- app/controllers/administrate/articles_controller.rb
+++ app/controllers/administrate/articles_controller.rb
@@ -4,6 +4,7 @@ module Administrate
   class ArticlesController < AdministrateController
     before_action :set_article, only: [:show, :edit, :update, :destroy, :destroy_cover_image]
     before_action :set_categories, only: [:new, :edit, :show]
+    before_action :set_authors, only: [:new, :edit, :show]
 
     # GET /articles or /articles.json
     def index
@@ -78,18 +79,22 @@ module Administrate
 
     private
 
+    def set_categories
+      @categories = Category.all
+    end
+
+    def set_authors
+      @authors = Author.all
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.friendly.find(params[:id])
     end
 
-    def set_categories
-      @categories = Category.all
-    end
-
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :body, :cover_image, :category_id)
+      params.require(:article).permit(:title, :body, :cover_image, :category_id, :author_id)
     end
   end
 end

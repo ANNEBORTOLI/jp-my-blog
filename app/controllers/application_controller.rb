0a36ca075ffc8d8dcccb0a438ea8e4bb2eb6e4e2
Anne Bortoli <annebortoli@gmail.com>
2024-07-23 18:05:26 -0300

    redirect user after login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_footer_data
+  before_action :store_user_location!, if: :storable_location?
 
   def set_footer_data
     return if params[:controller].split("/").first == "administrate"
@@ -9,5 +10,36 @@ class ApplicationController < ActionController::Base
     @footer_categories_1 = Category.all.order(created_at: :asc).limit(4)
     @footer_categories_2 = Category.all.order(created_at: :desc).limit(4)
     @footer_articles = Article.all.sample(4)
+  end
+
+  private
+
+  # Its important that the location is NOT stored if:
+  # - The request method is not GET (non idempotent)
+  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
+  #    infinite redirect loop.
+  # - The request is an Ajax request as this can lead to very unexpected behaviour.
+  def storable_location?
+    request.get? &&
+      is_navigational_format? &&
+      !devise_controller? &&
+      !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  # To redirect to the stored location after the user signs in
+  # you would override the after_sign_in_path_for method:
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
+  # To redirect to the stored location after the user signs out
+  # you would override the after_sign_out_path_for method:
+  def after_sign_out_path_for(resource_or_scope)
+    request.referer if resource_or_scope == :user
   end
 end

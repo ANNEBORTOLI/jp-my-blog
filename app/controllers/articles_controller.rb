707a96936a4de6b53c0951e5ca4af0bd2ebde38f
Anne Bortoli <annebortoli@gmail.com>
2024-07-23 16:57:22 -0300

    added new comment and order comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,14 @@ class ArticlesController < ApplicationController
     @article = Article.includes(:category, :author, comments: :user).friendly.find(params[:id])
     @other_articles = Article.all.sample(3)
     # @other_articles = Article.all.take(3) # same article always
+    @comments = comments_sorted_by
+  end
+
+  private
+
+  def comments_sorted_by
+    return @article.comments.order(created_at: :desc) if params[:sort_by] == "more_recents"
+
+    @article.comments.order(created_at: :asc)
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  def index
-    @article = Article.find(params[:article_id])
-    @comments = @article.comments
+  def create
+    @article = Article.friendly.find(params[:article_id])
+    @article.comments.create!(comments_params)
 
-    p("*" * 100)
-    p(@comments)
-    p("*" * 100)
+    redirect_to(article_path(@article))
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:body)
   end
 end
-
-# "fdbbc9a4-354d-427f-b08f-3ea57946c881"

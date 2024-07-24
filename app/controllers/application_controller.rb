643dfe8446a2f4cd92565f323a639ac384181fdd
Anne Bortoli <annebortoli@gmail.com>
2024-07-24 19:40:21 -0300

    added markdown to administrade form with StackEdit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,5 +41,6 @@ class ApplicationController < ActionController::Base
   # you would override the after_sign_out_path_for method:
   def after_sign_out_path_for(resource_or_scope)
     request.referer if resource_or_scope == :user
+    super # logout of the /administrate goes back to home
   end
 end
